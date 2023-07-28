0ceb5343d3baac3f696096607624512410de0c67
MythiliPalanisamy <102500645+MythiliPalanisamy@users.noreply.github.com>
2023-07-28 14:21:44 +0200

    creating docker

--- app.py
+++ app.py
@@ -1,49 +1,76 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Optional, Union
-from enum import Enum
+from fastapi import FastAPI,Request, status
+from pydantic import BaseModel, Field
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
+from typing import Optional
 from src.prediction import *
 from src.preprocessing import *
 import json
 
-
 app = FastAPI()
 
-"""class BuildingConditionEnum(str, Enum):
-    GOOD = 'good'
-    TO_RENOVATE = 'to renovate'"""
+# possible input for user reference (for schema)
+
+Property = ['apartment', 'bungalow', 'castle', 'chalet', 'country-cottage','duplex', 'exceptional-property', 'farmhouse', 'flat-studio','ground-floor', 'house', 'kot', 'loft', 'manor-house', 'mansion','mixed-use-building', 'other-property', 'penthouse','service-flat', 'town-house', 'triplex', 'villa']
+EnergyClass = ['A', 'A+', 'A++', 'B', 'C',  'D', 'E', 'F', 'G']
+KitchenType = ['Hyper equipped', 'Installed', 'Not installed','Semi equipped', 'USA hyper equipped', 'USA installed','USA semi equipped', 'USA uninstalled']
+HeatingType = ['Carbon', 'Electric', 'Fuel oil', 'Gas', 'Pellet', 'Solar','Wood']
+province = ['Antwerp', 'Brussels Capital Region', 'East Flanders','Flemish Brabant', 'Hainaut (West)', 'Limburg', 'Liège','Luxembourg (shared with Eastern Hainaut)', 'Namur','Walloon Brabant', 'West Flanders']
+BuildingCondition = ['As new', 'Good', 'Just renovated', 'To be done up','To renovate', 'To restore']
+
+
 
 class Data(BaseModel):
-        type_of_property : Optional[str] = None
-        postal_code : Optional[int] = None
-        energy_class : Optional[str] = None
-        bedrooms : Optional[int] = None
-        bathrooms : Optional[int] = None
-        toilets : Optional[int] = None
-        number_of_frontages : Optional[int] = None
-        kitchen_type : Optional[str] = None
-        heating_type : Optional[str] = None
-        surface_of_the_plot : Optional[int] = None
-        living_room_surface : Optional[int] = None
-        province : Optional[str] = None
-        building_condition : Optional[str] = None
+        type_of_property: Optional[str] = Field(None, description = f"can be one of the items in following list \n {Property}")
+        postal_code : Optional[int] = Field(None, description = "should be Belgium postal code")
+        energy_class : Optional[str] = Field(None, description = f"can be one of the items in following list \n {EnergyClass} ")
+        bedrooms : Optional[int] = Field(None, description = "Enter number of bedrooms in the house")
+        bathrooms : Optional[int] = Field(None, description = "Enter number of bathrooms in the house")
+        toilets : Optional[int] = Field(None, description = "Enter number of toilets in the house")
+        number_of_frontages : Optional[int] = Field(None, description = "Enter number of frontages in the house")
+        kitchen_type : Optional[str] = Field(None, description = f"can be one of the items in following list \n {KitchenType}")
+        heating_type : Optional[str] = Field(None, description = f"can be one of the items in following list \n {HeatingType}")
+        surface_of_the_plot : Optional[int] = Field(None, description = "Enter surface of the plot")
+        living_room_surface : Optional[int] = Field(None, description = "Enter living room surface of the plot")
+        province : Optional[str] = Field(None, description = f"can be one of the items in following list \n {province}")
+        building_condition : Optional[str] = Field(None, description = f"can be one of the items in following list \n {BuildingCondition}")
 
 @app.get('/')
 def index():
     return {'''Enter the following details to predict the value of house:    1) Type of property    2) postal code    3) Bedrooms    4) Energy class    5) Surface of the plot    6) Living room surface    7) Number of frontages    8) Building condition     9) Bathrooms     10) Toilets    11) Kitchen type    12) Heating type    13) province '''}
 
+#handling errors
+@app.exception_handler(RequestValidationError)
+def validation_exception_handler(request: Request, exc: RequestValidationError):
+
+    if exc.errors()[0]['type']=='string_type':
+
+        error_location = exc.errors()[0]['loc']
+        error_msg = exc.errors()[0]['msg']
+        input_value = exc.errors()[0]['input']
+        error_message = f" Error Message: {error_msg} instead got input as {input_value} at {error_location}"
+
+        return PlainTextResponse(content=error_message, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+    else:
+         
+         error_message = "Input should be valid number instead got string" 
+      
+         return PlainTextResponse(content=error_message, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 # preprocessing and prediction.py
 @app.post('/predict')
 def predict(property : Data):
-  
+
     property = json.loads(property.json())
     df = pd.DataFrame(property, index=[0])
-    print(df)
+    
     processed = preprocess(df)
-    price = predict_price(processed)
+    price = predict_forest_price(processed)
 
     return {'predicted_price ' : price[0]}
+
+
 
     
     
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,41 +1,36 @@
 import numpy as np
 import pandas as pd
 import pickle
-from src.prediction import *
-
 
 def preprocess(raw_dataframe):
     
-    # initialising catagorical columns which can be used later for one hot encoding
-    column=[ 'type_of_property', 'building_condition', 'kitchen_type',  'province','energy_class', 'heating_type',] # catagorical
+    """ For reference :
+    'one_hot_encoded_array' = encoding given column's with one hot encoder.
+    'ohe.categories_' = list of category in each column as separate array.
+    'categories' = coverting list of (names of) arrays as single array
+    'encoded_dataframe' = converting encoded array and names of array as single dataframe
+    'final' = concatenating encoded dataframe and original dataframe
+    """
+
+    # catagorical columns
+    column=[ 'type_of_property', 'building_condition', 'kitchen_type',  'province','energy_class', 'heating_type',]
     
-    # loading saved one hot encoding trained data
+    # loading one hot encoding trained data and minmax scaler
     ohe = pickle.load(open(r'C:\Users\Karthick Palanivel\Documents\GitHub\Price_prediction\models\ohe.pickle', 'rb'))
-    
-    # converting test data
+    minmax_scaler = pickle.load(open(r'C:\Users\Karthick Palanivel\Documents\GitHub\Price_prediction\models\minmax_scaler.pickle', 'rb'))
+
+    # using onehot encoder
     one_hot_encoded_array = ohe.transform(raw_dataframe[column]).toarray()
-
-    # 'ohe.categories_' gives list of category in each column as separate array. 
-    # concatenating it as single array
     categories = np.concatenate(ohe.categories_)
-
-    # creating new dataframe with one hot encoded array with respective categories
     encoded_dataframe = pd.DataFrame(one_hot_encoded_array, columns=categories)
 
     # droping encoded columns from original dataframe
     raw_dataframe = raw_dataframe.drop(columns=[ 'type_of_property', 'building_condition', 'kitchen_type',  'province','energy_class', 'heating_type','postal_code'])
-
-    # changing entire dataset into integer
-    raw_dataframe = raw_dataframe.astype(int)
-
-    # concatenating encoded dataframe and original dataframe
+    raw_dataframe = raw_dataframe.astype(int) 
     final = pd.concat([raw_dataframe,encoded_dataframe], axis=1)
  
-    # loading saved minmax scaler
-    minmax_scaler = pickle.load(open(r'C:\Users\Karthick Palanivel\Documents\GitHub\Price_prediction\models\minmax_scaler.pickle', 'rb'))
-
+    # using minmax scaler
     final[['surface_of_the_plot', 'living_room_surface']] = minmax_scaler.transform(final[[ 'surface_of_the_plot', 'living_room_surface']])
-
     processed=final.drop(columns=['G_F','NS','C_B','0'])
 
     return processed
